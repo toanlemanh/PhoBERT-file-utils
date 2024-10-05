b9d174c102dff8258d245ca330f5b6f7c44b10aa
toanlemanh <toanlemanh2003@gmail.com>
2024-10-05 09:09:24 +0700

    complete handling file integration

---
+++ vn_nlp_script.py
@@ -0,0 +1,36 @@
+import torch
+from transformers import AutoModel, AutoTokenizer
+from transformers import logging
+import os
+import re
+logging.set_verbosity_warning()
+from vn_wordsegment_script import vn_nlp
+
+phobert = AutoModel.from_pretrained("vinai/phobert-base-v2")
+tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base-v2")
+
+# INPUT TEXT MUST BE ALREADY WORD-SEGMENTED!
+# Handle word-sengment taskd for Vn
+wordsegemnt_file = vn_nlp()
+
+def encode_sentences_from_file(input_file_path, tokenizer):
+    with open(input_file_path, 'r', encoding='utf-8') as infile:
+        content = infile.read()  # Read the entire content of the file
+
+    # Use regex to split the content into sentences based on valid punctuation marks
+    sentences = re.split(r'(?<=[.!?...;]) +', content)  # Split at sentence-ending punctuation
+
+    for sentence in sentences:
+        sentence = sentence.strip()  # Remove any surrounding whitespace or newlines
+        if sentence:  # Check if the sentence is not empty
+            encoded_tensor = torch.tensor([tokenizer.encode(sentence)])
+            with torch.no_grad():
+                features = phobert(encoded_tensor)  # Models outputs are now tuples
+                print(features)
+    #         encoded_sentences.append(encoded_tensor)  # Store the tensor in the list
+encode_sentences_from_file(input_file_path = wordsegemnt_file, tokenizer = tokenizer)   
+
+
+## With TensorFlow 2.0+:
+# from transformers import TFAutoModel
+# phobert = TFAutoModel.from_pretrained("vinai/phobert-base")
---
+++ vn_wordsegment_script.py
@@ -0,0 +1,26 @@
+import py_vncorenlp
+import os
+import re
+from helpers.check_models import check_models
+from helpers.reformat_wordsegment_output import reformat_wordsegment_output as process_wordsegment
+
+# Automatically download VnCoreNLP components from the original repository
+# and save them in some local machine folder
+def vn_nlp():
+    abspath = os.path.abspath(".")
+    input_file = abspath + '/output/output_corpus.txt'
+    output_file = abspath +'/output/pre_output.txt'
+    wordsegment_file = abspath + "/output/final_output.txt"
+    file_path = abspath +"/VNCoreNLP-1.2.jar"
+    dir_path = abspath +"/models"
+    if ( not check_models(file_path, dir_path) ) :
+        py_vncorenlp.download_model(save_dir= abspath+'/vncorenlp')
+
+    # Load the word and sentence segmentation component
+    rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=abspath+'/vncorenlp')
+
+    # Receive the corpus output file (after corpus reader) and generate a word_segment file
+    rdrsegmenter.annotate_file(input_file= input_file, output_file= output_file)
+
+    # Reformat the content in file and generate a final file
+    return process_wordsegment(input_file_path=output_file, output_file_path= wordsegment_file)
